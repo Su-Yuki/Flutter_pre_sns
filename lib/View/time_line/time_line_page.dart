b80c2cd961d4800cf6c0117c5c464f06b996e1ad
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-11-01 23:21:57 +0900

    wip

---
+++ lib/View/time_line/time_line_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TimeLinePage extends StatelessWidget {
+  const TimeLinePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('time line')),
+    );
+  }
+}
